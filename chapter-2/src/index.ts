fc42d1fb95a2fe84096da3b9da4a3ba3cefb9cab
GRTheory <100547239+GRTheory@users.noreply.github.com>
2023-08-21 06:57:17 +0000

    init ts project

---
+++ chapter-2/src/index.ts
@@ -0,0 +1,10 @@
+type Color = 'Black' | 'White'
+type File = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H'
+type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8
+
+class Position {
+    constructor(
+        private file: File,
+        private rank: Rank
+    ){}
+}
